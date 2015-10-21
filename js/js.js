105600c93fbf2c134aaa050bc56ee58aa4474bd0
Ivan Leshchenko <vano@wnb.com.ua>
2015-10-21 21:25:39 +0300

    almost done

--- js/js.js
+++ js/js.js
@@ -9,4 +9,21 @@ $(document).ready(function() {
 			$('.food-text' + '-' + nextIndex).fadeIn(300);
 		}
 	});
+
+
+	$("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
 });
