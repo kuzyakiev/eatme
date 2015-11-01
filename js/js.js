80f0ad3f2d32f9e246e15b945deaeb67cc36d69d
Ivan Leshchenko <vano@wnb.com.ua>
2015-11-01 20:25:24 +0200

    ...

--- js/js.js
+++ js/js.js
@@ -12,20 +12,28 @@ $(document).ready(function() {
 
 
 	$("#owl-demo").owlCarousel({
- 
+
       navigation : true, // Show next and prev buttons
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
- 
+
       // "singleItem:true" is a shortcut for:
       // items : 1, 
       // itemsDesktop : false,
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
+
   });
+
+  $('.arrow-down').click(function(e){
+      e.preventDefault();
+      $.fn.fullpage.moveSectionDown();
+  });  
+
+
+
 
 
 
